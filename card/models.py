351497ab9cc9869a2a861dec21c72396cf3df2b1
Sharvari Kapse <sharvarikapse16@gmail.com>
2026-09-02 23:22:49 +0530

    Model create

--- card/models.py
+++ card/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    # img = models.ImageField(upload_to='products/' , blank=True)
 
 def __str__(self):
     return self.name
